c5f3908583a66cb26f210fe3171a06078b46dab8
MOOICT <MOOICT@DESKTOP-OQHBRE0>
2022-03-04 11:13:42 +0000

    Add project files.

---
+++ Maths Quiz Version 2 MOO ICT/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Maths_Quiz_Version_2_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumA = new System.Windows.Forms.Label();
+            this.lblSymbol = new System.Windows.Forms.Label();
+            this.lblNumB = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumA
+            // 
+            this.lblNumA.AutoSize = true;
+            this.lblNumA.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumA.Location = new System.Drawing.Point(75, 26);
+            this.lblNumA.Name = "lblNumA";
+            this.lblNumA.Size = new System.Drawing.Size(49, 33);
+            this.lblNumA.TabIndex = 0;
+            this.lblNumA.Text = "00";
+            // 
+            // lblSymbol
+            // 
+            this.lblSymbol.AutoSize = true;
+            this.lblSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSymbol.Location = new System.Drawing.Point(139, 26);
+            this.lblSymbol.Name = "lblSymbol";
+            this.lblSymbol.Size = new System.Drawing.Size(32, 33);
+            this.lblSymbol.TabIndex = 1;
+            this.lblSymbol.Text = "?";
+            // 
+            // lblNumB
+            // 
+            this.lblNumB.AutoSize = true;
+            this.lblNumB.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumB.Location = new System.Drawing.Point(187, 26);
+            this.lblNumB.Name = "lblNumB";
+            this.lblNumB.Size = new System.Drawing.Size(49, 33);
+            this.lblNumB.TabIndex = 2;
+            this.lblNumB.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(254, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "=";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(307, 26);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(49, 33);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Press Q for +, W for - and E for x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 158);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblNumB);
+            this.Controls.Add(this.lblSymbol);
+            this.Controls.Add(this.lblNumA);
+            this.Name = "Form1";
+            this.Text = "Maths Quiz Version 2";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumA;
+        private System.Windows.Forms.Label lblSymbol;
+        private System.Windows.Forms.Label lblNumB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Maths Quiz Version 2 MOO ICT/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maths_Quiz_Version_2_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        // www.mooict.com
+        // project based programming tutoials
+
+        int numA;
+        int numB;
+        int total;
+
+        Random rnd = new Random();
+        string[] Maths = {"Add", "Subtract", "Multiply" };
+
+        string secretAnswer;
+        string userChoice;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+
+            numA = rnd.Next(10, 20);
+            numB = rnd.Next(0, 9);
+
+            secretAnswer = Maths[rnd.Next(0, Maths.Length)];
+
+            switch (secretAnswer)
+            {
+                case "Add":
+                    total = numA + numB;
+                    break;
+
+                case "Subtract":
+                    total = numA - numB;
+                    break;
+
+                case "Multiply":
+                    total = numA * numB;
+                    break;
+            }
+
+            lblNumA.Text = numA.ToString();
+            lblNumB.Text = numB.ToString();
+            lblTotal.Text = total.ToString();
+            lblSymbol.Text = "?";
+        }
+
+        private void CheckAnswer()
+        {
+
+            if (userChoice == secretAnswer)
+            {
+                MessageBox.Show("Correct, Now try again!");
+                SetUpGame();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect, try again!");
+                lblSymbol.Text = "?";
+            }
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Q)
+            {
+                userChoice = "Add";
+                lblSymbol.Text = "+";
+                CheckAnswer();
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                userChoice = "Subtract";
+                lblSymbol.Text = "-";
+                CheckAnswer();
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                userChoice = "Multiply";
+                lblSymbol.Text = "x";
+                CheckAnswer();
+            }
+        }
+    }
+}
